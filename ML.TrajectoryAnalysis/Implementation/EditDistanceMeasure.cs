e150d121ff0136558f674d3778fca021d7dca7ba
vilsonluffy <vilson.espayos.lu@gmail.com>
2019-03-23 17:42:09 +0800

    Found a library that implements edit distance measure

---
+++ ML.TrajectoryAnalysis/Implementation/EditDistanceMeasure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML.TrajectoryAnalysis.Implementation
+{
+    public class EditDistanceMeasure : ISimilarityMeasure
+    {
+        public double MeasureSimilarity(List<Trajectory> a, List<Trajectory> b)
+        {
+            string word1 = "minimum";
+            string word2 = "maximum";
+
+           return (double) MinimumEditDistance.Levenshtein.CalculateDistance(word1, word2, 2);
+        }
+    }
+}
