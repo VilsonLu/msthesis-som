d14080ff63990b762865686b41c5579cb4798118
vilsonluffy <vilson.espayos.lu@gmail.com>
2019-04-05 00:39:08 +0800

    Fixed implementation for SSOM and KNN labeller

--- SOMLibrary/SSOM.cs
+++ SOMLibrary/SSOM.cs
@@ -19,7 +19,7 @@ namespace SOMLibrary
 
         #endregion
 
-        public SSOM() 
+        public SSOM()
         {
             Width = 0;
             Height = 0;
@@ -27,6 +27,7 @@ namespace SOMLibrary
             Epoch = 1;
             Map = new Node[Width, Height];
             Regions = new List<Region>();
+            K = 5;
         }
 
         public SSOM(int x, int y) : base(x, y)
@@ -69,21 +70,50 @@ namespace SOMLibrary
                 currentHeight = region.Height + startCol;
             }
 
-            for (int row = startRow; row < currentWidth; row++)
+
+            if(region != null)
             {
-                for (int col = startCol; col < currentHeight; col++)
+                for (int row = startRow; row < currentWidth; row++)
                 {
-                    Node currentNode = Map[row, col];
-                    double currentDistance = currentNode.GetDistance(instance);
-
-                    if (currentDistance < bestDistance)
+                    for (int col = startCol; col < currentHeight; col++)
                     {
-                        bestDistance = currentDistance;
-                        bestNode = currentNode;
-                    }
+                        Node currentNode = Map[row, col];
+                        double currentDistance = currentNode.GetDistance(instance);
 
+                        if (currentDistance < bestDistance)
+                        {
+                            bestDistance = currentDistance;
+                            bestNode = currentNode;
+                        }
+
+                    }
                 }
             }
+            else
+            {
+                for (int row = startRow; row < currentWidth; row++)
+                {
+                    for (int col = startCol; col < currentHeight; col++)
+                    {
+                        Node currentNode = Map[row, col];
+
+                        if (IsInAnyRegion(row, col))
+                        {
+                            continue;
+                        }
+
+                        double currentDistance = currentNode.GetDistance(instance);
+
+                        if (currentDistance < bestDistance)
+                        {
+                            bestDistance = currentDistance;
+                            bestNode = currentNode;
+                        }
+
+                    }
+                }
+            }
+           
 
             return bestNode;
         }
@@ -99,7 +129,7 @@ namespace SOMLibrary
             }
 
             // TODO: Add a validation to check if the region will be out of bound
-            
+
             if (Regions.Any(x => x.Label == label))
             {
                 var index = Regions.FindIndex(x => x.Label == label);
@@ -120,6 +150,37 @@ namespace SOMLibrary
             var coordinate = new Coordinate(x, y);
             var isInAnyRegion = Regions.Any(r => r.IsWithinRegion(coordinate));
             return isInAnyRegion;
+        }
+
+        /// <summary>
+        /// Checks if the node is in a region based on label
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="label"></param>
+        /// <returns></returns>
+        private bool IsNodeInRegion(int x, int y, string label)
+        {
+            // If no region, always return false
+            if (Regions == null || Regions.Count == 0)
+            {
+                return false;
+            }
+
+            // Find the region based on the label
+            var regionLabel = Regions.FirstOrDefault(r => r.Label == label);
+
+            // If no region for the specific label, return false
+            if (regionLabel == null)
+            {
+                return false;
+            }
+
+            var coordinate = new Coordinate(x, y);
+            var isInRegion = regionLabel.IsWithinRegion(coordinate);
+
+            return isInRegion;
+
         }
 
         public bool IsValidRegion(Region region)
